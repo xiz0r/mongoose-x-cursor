a5f304500dcd2b891e7519ba3f332ab85686b2f9
Juan Colo <juan@genuinebee.com>
2023-12-13 15:50:49 -0300

    feat: create barrels

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './aggregation-pagination-logic';
+export * from './interfaces';
+export * from './memory-cache-provider';
+export * from './mongoose-pagination-plugin';
+export * from './pagination-service';
+export * from './pagintaion-logic';
---
+++ src/interfaces/index.ts
@@ -0,0 +1,4 @@
+export * from './cache-provider';
+export * from './pagination-aggregate';
+export * from './pagination-models';
+export * from './pagination';
